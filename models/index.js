e034a67f0f369134df58af9864393330513d5295
cdplourde <cdplourde@gmail.com>
2018-06-27 20:58:13 -0700

    updated controllers and models

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  Calendar: require("./calendar"),
+  User: require("./user")
+}
---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const calendarsController = require("../controllers/calendarsController.js");
+const usersController = require("../controllers/usersController")
+
+
