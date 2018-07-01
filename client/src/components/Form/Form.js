8f82eed6fba65c7b6fe81a6e3e8fd738b2c30900
cfdahill <cfdahill@gmail.com>
2018-07-01 15:43:21 -0700

    making form console.log user input

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,29 +2,72 @@ import React from "react";
 
 class Form extends React.Component {
   state = {
-    Watch: [<input name= "watch" key="watch0"></input>] //this is to add multiple watch locations
+    Watch: [<input name= "watch" key="watch0"  arrayPos={0} onchange={this.handleChange}/>], //this is to add multiple watch locations
+    _Events: {},
+    title: "",
+    date: "",
+    time: "",
+    link: "",
+    info: "",
+    watch: []
   }
 
   //onClick event to add multiple watch locations
   addWatch = event => {
     event.preventDefault();
     const Watch = this.state.Watch;
-    this.setState({Watch: Watch.concat(<input name= "watch" key={`watch${Watch.length}`}></input>)});
+    this.setState({Watch: Watch.concat(<input name="watch" arrayPos={Watch.length} key={`watch${Watch.length}`}></input>)});
     console.log(this.state.Watch);
   }
 
+  handleInputChange = event => {
+    const name = event.target.name;
+    let value = event.target.value;
+    if(name.startsWith("watch")) {
+      const i = event.target.arrayPos;
+      
+    }
+    this.setState({[name]: value});
+    
+  }
 
   render() {
   return (
     <div className="Form" id={this.props.id}>
-      Event Name: <input name="eventName" />
-      Date: <input name="date" />
-      Time: <input name="time" />
-      Where to Watch? {this.state.Watch}
-      <button onClick={this.addWatch}>+ location</button>
-      Website: <input name="website" />
+      Event Name: 
+      <input 
+        name="title" 
+        value={this.state.title} 
+        onChange={this.handleInputChange} 
+      />
+      Date: 
+      <input 
+        name="date" 
+        value={this.state.date} 
+        onChange={this.handleInputChange}
+      />
+      Time: 
+      <input 
+        name="time" 
+        value={this.state.time}  
+        onChange={this.handleInputChange} 
+      />
+      Where to Watch? 
+      {this.state.Watch}
+      <button onClick={this.addWatch}>
+        + location
+      </button>
+      Website: 
+      <input 
+        name="link" 
+        value={this.state.link} 
+        onChange={this.handleInputChange}
+      />
       Additional Information:
-      <textarea name="addInfo"/>
+      <textarea 
+        name="info" 
+        value={this.state.info} 
+        onChange={this.handleInputChange}/>
       <button 
         onClick={(event) => {
           console.log(this.props.id);
--- client/src/components/FormWrapper/FormWrapper.js
+++ client/src/components/FormWrapper/FormWrapper.js
@@ -3,18 +3,40 @@ import Form from "../Form";
 
 class FormWrapper extends React.Component {
   state = {
-    Forms: [],
-    
+    Forms: [],  //This is to create multiple <Forms /> on the screen
+    scheduleName: "",
+    public: true,
+    schedule: {}, //might need this to push schedule to the db
+    savedEvents: []
   }
 
-  
+  //this will change this.state.public to private if private is checked
+  setPublic = event => {
+    if(event.target.value === "Private") {
+      this.setState({public: false});
+    } 
+  }
+
+  //will update scheduleName while inputting scheduleName, this can be used to show user that they are inputting a unique scheduleName (will implement later)
+  handleChange = event => {
+    this.setState({scheduleName: event.target.value});
+  }
+
   //methods to save all form info to database
-  //This will tie in the SAVE button
+  saveEvents = () => {
+    const schedule = {
+      title: this.state.scheduleName,
+      public: this.state.public,
+      author: "Need to get this from cookies, will implement later",
+      savedEvents: this.state.savedEvents,
+    };
+
+    console.log(schedule);
+  }
 
   //Create additional event forms
-  moreEvents = event => {
+  moreEvents = () => {
     //might want to insert a unique schedule name check here so no forms appear until it is unique.  If not, start with one event form
-    event.preventDefault();
     const Forms = this.state.Forms;
     this.setState({Forms: Forms.concat(<Form 
       key={`form${Forms.length}`}
@@ -34,11 +56,16 @@ class FormWrapper extends React.Component {
       <div id = "FormWrapper" >
         <h1>Create a new schedule</h1>
         <form>
-          Schedule Name: <input name="scheduleName"></input> <br />
-          <input type="radio" id="public" name="pub-priv" defaultChecked />
-          <label htmlFor="public">Public</label> <br />
-          <input type="radio" id="private" name="pub-priv" />
-          <label htmlFor="private">Private</label> <br />
+          Schedule Name: 
+          <input 
+            value={this.state.scheduleName}
+            onChange={this.handleChange} 
+            name="scheduleName" 
+          /> <br />
+          <div onChange={this.setPublic.bind(this)}>
+            <input type="radio" id="public" name="pub-priv" value="Public" />Public
+            <input type="radio" id="private" name="pub-priv" value="Private" />Private
+          </div>
           <p>Give the following people administrative access: <br />
           (seperate emails with a space or press enter after each one)</p>
           <textarea />
@@ -46,14 +73,25 @@ class FormWrapper extends React.Component {
           {this.state.Forms}
           <button 
             className="moreEvents"
-            onClick={this.moreEvents}
+            onClick={ event => {
+              event.preventDefault();
+              this.moreEvents();
+            }}
             >
               + Event
           </button>
           <p>Include the emails of anybody you would like to share this calendar with.  This is the only way other users may find a private calendar.
           (seperate emails with a space or press enter after each one)</p>
           <textarea name="shareList" />
-          <button className="saveSched">Save and Publish</button>
+          <button 
+            className="saveSched"
+            onClick = {event => {
+              event.preventDefault();
+              this.saveEvents();
+            }}
+          >
+            Save and Publish
+          </button>
         </form>
       </div>
     )
