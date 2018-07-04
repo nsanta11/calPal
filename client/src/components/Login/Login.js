b1a8b29c7f825b63dddc302ac22644a6b64da469
cfdahill <cfdahill@gmail.com>
2018-07-04 12:46:52 -0700

    login page is stylized

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { Button, Icon } from 'semantic-ui-react'
 
 const Login = props =>
     <div>
@@ -7,16 +8,22 @@ const Login = props =>
             <div className="calPal">calPal</div>
             <form action="/login" method="post">
                 <div>
-                    <label>Username:</label>
+                    <label>Username: </label>
                     <input type="text" name="username" />
                 </div>
                 <div>
-                    <label>Password:</label>
+                    <label>Password:  </label>
                     <input type="password" name="password" />
                 </div>
-                <div>
+                <Button animated type="submit" className="loginButton">
+                  <Button.Content visible>Log In</Button.Content>
+                  <Button.Content hidden>
+                    <Icon name='right arrow' />
+                  </Button.Content>
+                </Button>
+                {/* <div>
                     <input type="submit" value="Log In" />
-                </div>
+                </div> */}
             </form>
         </div>
         {props.children}
