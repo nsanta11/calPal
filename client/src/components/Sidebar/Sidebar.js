20d007dc444da4a9511f80258fc5d9c2f4091ceb
Chay Dahill <cfdahill@gmail.com>
2018-07-07 15:31:43 -0700

    added semantic elements

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { Dropdown } from 'semantic-ui-react'
+import { Search, Grid, Header, Segment } from 'semantic-ui-react'
 
 const Sidebar = () => {
   return (
     <div>
+      Dropdown menu of schedules
+    <Dropdown placeholder='Select Schedule' fluid search selection options='a, b, c' />
+
+    Search bar of schedules
+
+          <Search
+            // loading={isLoading}
+            // onResultSelect={this.handleResultSelect}
+            // onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+            // results={results}
+            // value={value}
+            // {...this.props}
+          />
+        
+        
+
+  
     SIDEBAR
     SIDEBAR
     SIDEBAR
