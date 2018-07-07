ea6a4e842965042ad6094ddb0a8a51883abb511c
cfdahill <cfdahill@gmail.com>
2018-07-07 11:26:45 -0700

    datepicker in form is now working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import CalendarWrapper from "./components/CalendarWrapper";
-import FormWrapper from "./components/FormWrapper";
+import Form from "./components/Form";
 import Login from "./components/Login";
 import './App.css';
 
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/calendar" component={CalendarWrapper} />
-          <Route exact path="/create" component={FormWrapper} />
+          <Route exact path="/create" component={Form} />
           <Route exact path="/" component={Login} />
         </div>
       </Router>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,96 +1,187 @@
 import React from "react";
+import "./Form.css";
+import Datetime from "react-datetime";
+import 'react-datetime/css/react-datetime.css';
 
 class Form extends React.Component {
   state = {
-    Watch: [], //this is to add multiple watch locations
-    _Event: {
-    title: "",
-    date: "",
-    time: "",
-    link: "",
-    info: "",
-    watch: []
-    }
+    scheduleName: "",
+    public: true,
+    //the following are to create multiple events
+    eventsArray: [],
+    eventsCount: 0,
+    //the following are to create multiple Wheres per event
+    watchCount: 0
   }
 
-  //onClick event to add multiple watch locations
-  addWatch = event => {
-    event.preventDefault();
-    const Watch = this.state.Watch;
-    this.setState({watch: this.state._Event.watch.push(``),
-      Watch: Watch.concat(<input 
-        name={`watch[${Watch.length}]`} 
-        arraypos={Watch.length} 
-        value={this.state._Event.watch[Watch.length]} 
-        key={`watch${Watch.length}`} 
-        onChange={this.handleInputChange} 
-      />
-    )});
-    console.log(this.state._Event.watch);
+  //will update scheduleName while inputting scheduleName, this can be used to show user that they are inputting a unique scheduleName (will implement later)
+  handleChange = event => {
+    this.setState({scheduleName: event.target.value});
   }
 
-  //will record all changes as they happen since Form does not know when the save button will be pushed, it will always be ready
-  handleInputChange = event => {
-    const name = event.target.name;
-    let value = event.target.value;
-    if(name.startsWith("watch")) {
-      const i = event.target.arrayPos;
-      let watch = this.state._Event.watch;
-      watch[i] = value;
-      this.setState({...this.state._Event, watch: watch});
-    }
-    else {
-      this.setState({...this.state._Event, [name]: value});
-    }
+  //this will change this.state.public to private if private is checked
+  setPublic = event => {
+    if(event.target.value === "Private") {
+      this.setState({public: false});
+    } 
   }
 
+  //methods to save all form info to database
+  saveEvents = () => {
+    const schedule = {
+      title: this.state.scheduleName,
+      public: this.state.public,
+      author: "Need to get this from cookies, will implement later",
+      savedEvents: this.state.eventsArray,
+    };
+
+    console.log(schedule);
+  }
+
+  //Create additional event forms
+  moreEvents = () => {
+    let eventsCount = this.state.eventsCount;
+    const eventsArray=this.state.eventsArray;
+    eventsArray.push({
+      title: "",
+      date: "",
+      watch: [],
+      link: "",
+      info: ""
+    });
+    this.setState({
+      eventsArray: eventsArray,
+      eventsCount: ++eventsCount
+    });
+  }
+
+  moreWatch = (index) => {
+    // console.log(this.state.eventsArray[index].watch);
+    let watch = this.state.eventsArray;
+    watch[index].watch.push("");
+    // console.log(watch);
+    this.setState({eventsArray: watch});
+  }
+
+  handleInputChange = (index, e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    let eventsArray = this.state.eventsArray;
+    eventsArray[index][name] = value;
+    this.setState({eventsArray});
+  }
+
+  handleWatchChange = (index, Windex, e) => {
+    let value = e.target.value;
+    let eventsArray = this.state.eventsArray;
+    console.log(eventsArray[index].watch[Windex]);
+    eventsArray[index].watch[Windex] = value;
+    this.setState({eventsArray});
+    console.log(this.state.eventsArray);
+  }
+
+  handleDateChange = (index, e) => {
+    console.log(e._d);
+    let eventsArray = this.state.eventsArray;
+    eventsArray[index].date = e._d;
+    this.setState({eventsArray});
+  }
+  
   render() {
-  return (
-    <div className="Form" id={this.props.id}>
-      Event Name: 
-      <input 
-        name="title" 
-        value={this.state.title} 
-        onChange={this.handleInputChange} 
-      />
-      Date: 
-      <input 
-        name="date" 
-        value={this.state.date} 
-        onChange={this.handleInputChange}
-      />
-      Time: 
-      <input 
-        name="time" 
-        value={this.state.time}  
-        onChange={this.handleInputChange} 
-      />
-      Where to Watch? 
-      {this.state.Watch}
-      <button onClick={this.addWatch}>
-        + location
-      </button>
-      Website: 
-      <input 
-        name="link" 
-        value={this.state.link} 
-        onChange={this.handleInputChange}
-      />
-      Additional Information:
-      <textarea 
-        name="info" 
-        value={this.state.info} 
-        onChange={this.handleInputChange}/>
-      <button 
-        onClick={(event) => {
-          console.log(this.props.id);
-          event.preventDefault()
-          this.props.deleteEvent(this.props.id)
-        }}
-      >
-          Delete
-      </button>
-    </div>
+    return ( 
+      <div id = "FormWrapper" >
+        <h1>Create a new schedule</h1>
+        <form>
+        Schedule Name:  
+          <input 
+            value={this.state.scheduleName}
+            onChange={this.handleChange} 
+            name="scheduleName" 
+          /> <br />
+          <div onChange={this.setPublic.bind(this)}>
+            <input type="radio" id="public" name="pub-priv" value="Public" />Public
+            <input type="radio" id="private" name="pub-priv" value="Private" />Private
+          </div>
+          <p>Give the following people administrative access: <br />
+          (seperate emails with a space or press enter after each one)</p>
+          <textarea />
+          {this.state.eventsArray.map((event, index) =>
+                <div className="Form" id={index} key={index}>
+                Event Name: 
+                <input 
+                  name="title" 
+                  value={event.title} 
+                  onChange={(e) => this.handleInputChange(index, e)} 
+                />
+                Date: 
+                <Datetime 
+                  name="date" 
+                  // value={event.date}
+                  className="date" 
+                  onChange={(e) => this.handleDateChange(index,e)}
+                />
+                {/* <input 
+                  name="date" 
+                  value={event.date} 
+                  onChange={(e) => this.handleInputChange(index, e)} 
+                /> */}
+                 Where to Watch?
+                {event.watch.map((Wevent, Windex) => 
+                  //Windex is short for watch index, need both that and the form index
+                  <input id={`watch${Windex}`}
+                    name={Wevent}
+                    value={Wevent} 
+                    key={`watch${Windex}`} 
+                    onChange={(e) => this.handleWatchChange(index, Windex, e)} 
+                  /> 
+                )}
+
+                <button 
+                  className="moreWatch"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    this.moreWatch(index);
+                  }}
+                >
+                  + location
+                </button>
+                Website: 
+                <input 
+                  name="link" 
+                  value={event.link} 
+                  onChange={(e) => this.handleInputChange(index, e)} 
+                />
+                Additional Information: 
+                <textarea 
+                  name="info" 
+                  value={event.info} 
+                  onChange={(e) => this.handleInputChange(index, e)} 
+                />
+              </div>
+          )}
+          <button 
+            className="moreEvents"
+            onClick={ event => {
+              event.preventDefault();
+              this.moreEvents();
+            }}
+            >
+              + Event
+          </button>
+          <p>Include the emails of anybody you would like to share this calendar with.  This is the only way other users may find a private calendar.
+          (seperate emails with a space or press enter after each one)</p>
+          <textarea name="shareList" />
+          <button 
+            className="saveSched"
+            onClick = {event => {
+              event.preventDefault();
+              this.saveEvents();
+            }}
+          >
+            Save and Publish
+          </button>
+        </form>
+      </div>
     )
   }
 }
