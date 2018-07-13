92481e46c06f1b5966c3a2031fddaa49eb34cf7e
cfdahill <cfdahill@gmail.com>
2018-07-13 12:45:08 -0700

    modal working for user content, not for api content

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -2,20 +2,33 @@ import React from "react";
 import FullCalendar from 'fullcalendar-reactwrapper';
 import 'fullcalendar-reactwrapper/dist/css/fullcalendar.min.css';
 import './Calendar.css';
+import { Modal } from "semantic-ui-react";
 
 class Calendar extends React.Component {
 
   state = {
-    events: [
-      {
-        title: 'Dbacks',
-        allDay: false,
-        start: "2018-07-09T16:00:00",
-      }
-    ]
+    title: '',
+    date: '',
+    watch: [],
+    link: '',
+    info: '',
+    open: false,
   }
 
-  //create modal when event is clicked on for more information.  Modals are part of semantic
+  moreInfo = (e) => {
+    console.log(e.title);
+   this.setState({
+     title: e.title,
+     date: e.date,
+     watch: e.watch,
+     link: e.link,
+     info: e.info,
+     open: true
+   });
+   console.log(this.state);
+  }
+
+  close = () => this.setState({ open: false });
 
   render(props) {
     return (
@@ -30,7 +43,23 @@ class Calendar extends React.Component {
             right: 'month, basicWeek, basicDay, list'
           }}
           events = {this.props.onScheduleChange}
+          eventClick = {(e)=>this.moreInfo(e)}
         />
+        <Modal 
+          open={this.state.open}
+        >
+          <Modal.Content>
+            <Modal.Description>
+              <h1>{this.state.title}</h1>
+              <p>Date: {this.state.date}</p>
+              <p>Website: <a href={`${this.state.link}`} target='blank'>{this.state.link}</a></p>
+              <p>Where to watch:</p>
+              {this.state.watch.map( (watch) => <p key={watch}>{watch}</p>)}
+              <p>Additional Information: {this.state.info}</p>
+              <button onClick={(e)=>this.close(e)}>close</button>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
       </div>
     )
   }
